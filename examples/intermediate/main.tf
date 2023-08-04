947078bd7ada99fcc3f23a1528e9d099acb57266
PatrykIti <77437371+PatrykIti@users.noreply.github.com>
2023-08-04 16:12:11 +0200

    Feature: Creating First Version of Terraform module for Azure Resource Groups (#1)
    * Created repo structure and imported tested module files
    
    * Correcting changelog pushing in workflow
    
    * Update workflows
    
    * Update README and CONTRIBUTING
    
    * Add
    
    * terraform-docs: automated action
    
    * docs: add badges [skip ci]
    
    * Update commit messages for badges
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ examples/intermediate/main.tf
@@ -0,0 +1,65 @@
+locals{
+    env                       = "dev1"
+    subscriptionInfix         = "c3d7d"
+    locationAcronymPrimary    = "eun"
+    locationPrimary           = "North Europe"
+    locationAcronymSecondary  = "euw"
+    locationSecondary         = "West Europe"
+    env_prefix_primary        = join("",[local.env,local.locationAcronymPrimary,local.subscriptionInfix])
+    env_prefix_secondary      = join("",[local.env,local.locationAcronymSecondary,local.subscriptionInfix])
+}
+
+#Primary Region
+module "rg_inf_primary" {
+    source = "https://dev.azure.com/PC-ITI/ECW%20-%20Engine%20Car%20Watcher/_git/terraform-azurerm-resource-group?ref=v1.0.0"
+
+    name      = join("-",[local.env_prefix_primary, "inf"])
+    location  = local.locationPrimary
+    tags      = {
+      environment               = "dev1"
+      environmentType           = "Development"
+      latestProvisioning        = "patryk.c@example.com"
+      latesteProvisioningDate   = "29-07-2023 19:34:00+00"
+      ProjectCode               = "2390-11-100"
+    }
+}
+#Secondary Region
+module "rg_inf_secondary" {
+    source = "https://dev.azure.com/PC-ITI/ECW%20-%20Engine%20Car%20Watcher/_git/terraform-azurerm-resource-group?ref=v1.0.0"
+
+    name      = join("-",[local.env_prefix_secondary, "inf"])
+    location  = local.locationSecondary
+    tags      = {
+      environment               = "dev1"
+      environmentType           = "Development"
+      latestProvisioning        = "patryk.c@example.com"
+      latesteProvisioningDate   = "29-07-2023 19:34:00+00"
+      ProjectCode               = "2390-11-100"
+    }
+}
+
+
+#Testing outputs from module
+#Only for showing how to get them after module execution
+#Outputs can be passed between modules of different resources
+
+#Primary region
+output "rg-inf-name-primary" {
+  value = module.rg_inf_primary.name
+}
+output "rg-inf-location-primary" {
+  value = module.rg_inf_primary.location
+}
+output "rg-inf-id-primary" {
+  value = module.rg_inf_primary.id
+}
+#Secondary region
+output "rg-inf-name-secondary" {
+  value = module.rg_inf_secondary.name
+}
+output "rg-inf-location-secondary" {
+  value = module.rg_inf_secondary.location
+}
+output "rg-inf-id-secondary" {
+  value = module.rg_inf_secondary.id
+}
---
+++ examples/simple/main.tf
@@ -0,0 +1,16 @@
+locals{
+    env_prefix = "dev1eunc3d7d"
+}
+
+module "rg_inf" {
+    source = "https://dev.azure.com/PC-ITI/ECW%20-%20Engine%20Car%20Watcher/_git/terraform-azurerm-resource-group?ref=v1.0.0"
+
+    name = join("-",[local.env_prefix, "inf"])
+    tags = {
+      environment               = "dev1"
+      environmentType           = "Development"
+      latestProvisioning        = "patryk.c@example.com"
+      latesteProvisioningDate   = "29-07-2023 19:34:00+00"
+      ProjectCode               = "2390-11-100"
+    }
+}
